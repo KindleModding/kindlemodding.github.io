92ad41727c669aa84beff1768777dd3f7b68e616
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-02-10 21:06:52 +0000

    Fix JB links

--- convert-kindle-serials.py
+++ convert-kindle-serials.py
@@ -282,7 +282,7 @@ generationMap = [
         "last_firmware": "LATEST",
         "platform": "Bellatrix3",
         "board": "Barolo",
-        "jailbreak": "<a href=\"LanguageBreak\">LanguageBreak</a>"
+        "jailbreak": "<a href=\"WinterBreak\">WinterBreak</a>"
     },
     # Kindle Basic 4, released October 12 2022 on FW 5.15.0
     {
@@ -294,7 +294,7 @@ generationMap = [
         "last_firmware": "LATEST",
         "platform": "Bellatrix",
         "board": "Cava",
-        "jailbreak": "<a href=\"LanguageBreak\">LanguageBreak</a>"
+        "jailbreak": "<a href=\"WinterBreak\">WinterBreak</a>"
     },
     # Kindle PaperWhite 5, released October 27 2021 on FW 5.14.0
     {
@@ -306,7 +306,7 @@ generationMap = [
         "last_firmware": "LATEST",
         "platform": "Bellatrix",
         "board": "Malbec",
-        "jailbreak": "<a href=\"LanguageBreak\">LanguageBreak</a>"
+        "jailbreak": "<a href=\"WinterBreak\">WinterBreak</a>"
     },
     # Kindle Oasis 3, released July 24 2019 on FW 5.12.0
     {
@@ -318,7 +318,7 @@ generationMap = [
         "last_firmware": "LATEST",
         "platform": "Zelda",
         "board": "Stinger",
-        "jailbreak": "<a href=\"WatchThis\">WatchThis</a>"
+        "jailbreak": "<a href=\"WinterBreak\">WinterBreak</a>"
     },
     # Kindle Basic 3, released April 10 2019 on FW 5.1x.y
     {
@@ -330,7 +330,7 @@ generationMap = [
         "last_firmware": "LATEST",
         "platform": "Rex",
         "board": "Moonshine",
-        "jailbreak": "<a href=\"WatchThis\">WatchThis</a>"
+        "jailbreak": "<a href=\"WinterBreak\">WinterBreak</a>"
     },
     # Kindle PaperWhite 4, released November 7 2018 on FW 5.10.0.1/5.10.0.2
     {
@@ -342,7 +342,7 @@ generationMap = [
         "last_firmware": "LATEST",
         "platform": "Rex",
         "board": "Jaeger",
-        "jailbreak": "<a href=\"LanguageBreak\">LanguageBreak</a>"
+        "jailbreak": "<a href=\"WinterBreak\">WinterBreak</a>"
     },
     # Kindle Oasis 2, released winter 2017 on FW 5.9.0.6
     {
@@ -354,7 +354,7 @@ generationMap = [
         "last_firmware": "5.16.2.1.1",
         "platform": "Zelda",
         "board": "Cognac",
-        "jailbreak": "<a href=\"WatchThis\">WatchThis</a>"
+        "jailbreak": "<a href=\"WinterBreak\">WinterBreak</a>"
     },
     # Kindle Basic 2, released summer 2016 on FW 5.8.0
     {
@@ -366,7 +366,7 @@ generationMap = [
         "last_firmware": "5.16.2.1.1",
         "platform": "Heisenberg",
         "board": "Eanab",
-        "jailbreak": "<a href=\"WatchThis\">WatchThis</a>"
+        "jailbreak": "<a href=\"WinterBreak\">WinterBreak</a>"
     },
     # Kindle Oasis, released late spring 2016 on FW 5.7.1.1
     {
@@ -378,7 +378,7 @@ generationMap = [
         "last_firmware": "5.16.2.1.1",
         "platform": "Duet",
         "board": "Whisky",
-        "jailbreak": "<a href=\"WatchThis\">WatchThis</a>"
+        "jailbreak": "<a href=\"WinterBreak\">WinterBreak</a>"
     },
     # Kindle PaperWhite 3, White, appeared w/ FW 5.7.3.1, released summer 2016 on FW 5.7.x?
     {
@@ -390,7 +390,7 @@ generationMap = [
         "last_firmware": "5.16.2.1.1",
         "platform": "Wario",
         "board": "Muscat",
-        "jailbreak": "<a href=\"LanguageBreak\">LanguageBreak</a>"
+        "jailbreak": "<a href=\"WinterBreak\">WinterBreak</a>"
     },
     # Kindle PaperWhite 3, released summer 2015 on FW 5.6.1
     {
@@ -402,7 +402,7 @@ generationMap = [
         "last_firmware": "5.16.2.1.1",
         "platform": "Wario",
         "board": "Muscat",
-        "jailbreak": "<a href=\"LanguageBreak\">LanguageBreak</a>"
+        "jailbreak": "<a href=\"WinterBreak\">WinterBreak</a>"
     },
     # Kindle Voyage, released fall 2014 on FW 5.5.0
     {
@@ -414,7 +414,7 @@ generationMap = [
         "last_firmware": "5.13.6",
         "platform": "Wario",
         "board": "Icewine",
-        "jailbreak": "<a href=\"LanguageBreak\">LanguageBreak</a>"
+        "jailbreak": "<a href=\"WinterBreak\">WinterBreak</a>"
     },
     # Kindle Basic (Pearl, Touch), released fall 2014 on FW 5.6.0
     {
@@ -426,7 +426,7 @@ generationMap = [
         "last_firmware": "5.12.2.2",
         "platform": "Wario",
         "board": "Bourbon",
-        "jailbreak": "<a href=\"WatchThis\">WatchThis</a>"
+        "jailbreak": "<a href=\"WinterBreak\">WinterBreak</a>"
     },
     # Kindle PaperWhite 2 (black bezel), released fall 2013 on FW 5.4.0
     {
@@ -438,7 +438,7 @@ generationMap = [
         "last_firmware": "5.12.2.2",
         "platform": "Wario",
         "board": "Pinot",
-        "jailbreak": "<a href=\"WatchThis\">WatchThis</a>"
+        "jailbreak": "<a href=\"WinterBreak\">WinterBreak</a>"
     },
     # Kindle PaperWhite (black bezel), released fall 2012 on FW 5.2.0
     {
