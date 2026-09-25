8f708e928b85ef9fc18d77b7270c932434b60756
yubaix <47avaa@gmail.com>
2026-09-24 19:50:53 -0500

    Add restoration of appreg.db to sanctuary's privesc.sh

--- static/sanctuary/privesc.sh
+++ static/sanctuary/privesc.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-logger "privesc.sh executed any failure is solely with the privesc now"
+cp /var/local/appreg.db.bak /var/local/appreg.db
+logger "sanctuary: appreg.db restored"
 curl -o /tmp/jb.so https://kindlemodding.org/sanctuary/aprivesc/jb.so
 chmod +x /tmp/jb.so
 lipc-set-prop com.lab126.system updateWaveform LD_PRELOAD=/tmp/jb.so
+logger "sanctuary: privesc.sh executed any failure is solely with the privesc now"
+
